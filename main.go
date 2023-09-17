83484db7f9cab23cdfaf40ef91cad9cfe0363c54
Robin Ketelbuters <robin.ketelbuters@gmail.com>
2023-09-17 20:54:13 +0200

    Move package into internal

--- main.go
+++ main.go
@@ -3,7 +3,7 @@ package main
 import (
 	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
 
-	"github.com/poseidon/terraform-provider-matchbox/matchbox"
+	"github.com/poseidon/terraform-provider-matchbox/internal/matchbox"
 )
 
 func main() {
